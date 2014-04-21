ba178245c5e31e7d1dd45dba3480998e99029421
huangbiao <hbiao68@yeah.net>
2014-04-21 18:58:06 +0800

    解决定义的接口类和实现类不在同一个目录下面，导致请求异常的问题

--- src/service/IComplexUserService.java
+++ src/service/IComplexUserService.java
@@ -7,8 +7,9 @@ import model.User;
 import model.Users;
 /**
  * <b>function:</b>定制客户端请求WebService所需要的接口
+ * endpointInterface 这个一定要说明，因为实现类和定义的接口不是在同一个目录下面，默认是找实现类的目录，这样就会出现请求无效的情况
  */
-@WebService
+@WebService(endpointInterface="service.imp.ComplexUserService")
 public interface IComplexUserService {
     
     public User getUserByName(@WebParam(name = "name") String name);
